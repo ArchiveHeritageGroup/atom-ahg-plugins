a31d4601b1cde19c5c038b03eef5888582476b8b
Your Name <you@example.com>
2025-12-27 18:54:12 +0200

    Dynamic dist bundle discovery - works with any build hash

--- arAHGThemeB5Plugin/templates/_layout_start.php
+++ arAHGThemeB5Plugin/templates/_layout_start.php
@@ -1,3 +1,38 @@
+<?php
+// Find dist bundles dynamically
+$distCss = sfConfig::get('sf_web_dir') . '/dist/css';
+$distJs = sfConfig::get('sf_web_dir') . '/dist/js';
+
+$ahgCss = '';
+$ahgJs = '';
+$vendorJs = '';
+
+// Find AHG CSS bundle
+if (is_dir($distCss)) {
+    foreach (glob($distCss . '/arAHGThemeB5Plugin.bundle.*.css') as $file) {
+        if (!strpos($file, '.map')) {
+            $ahgCss = '/dist/css/' . basename($file);
+            break;
+        }
+    }
+}
+
+// Find JS bundles
+if (is_dir($distJs)) {
+    foreach (glob($distJs . '/arAHGThemeB5Plugin.bundle.*.js') as $file) {
+        if (!strpos($file, '.map') && !strpos($file, '.LICENSE')) {
+            $ahgJs = '/dist/js/' . basename($file);
+            break;
+        }
+    }
+    foreach (glob($distJs . '/vendor.bundle.*.js') as $file) {
+        if (!strpos($file, '.map') && !strpos($file, '.LICENSE')) {
+            $vendorJs = '/dist/js/' . basename($file);
+            break;
+        }
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="<?php echo $sf_user->getCulture(); ?>" dir="<?php echo sfCultureInfo::getInstance($sf_user->getCulture())->direction; ?>">
   <head>
@@ -8,7 +43,9 @@
     <link rel="shortcut icon" href="<?php echo public_path('favicon.ico'); ?>">
     <link rel="stylesheet" href="/plugins/arAHGThemeB5Plugin/css/ahg-settings.css">
     <link rel="stylesheet" href="/plugins/arAHGThemeB5Plugin/css/ahg-generated.css">
-    <script defer src="/dist/js/vendor.bundle.7fd4785c4754082db86b.js"></script><script defer src="/dist/js/arAHGThemeB5Plugin.bundle.5143f3ea019729b8aaa0.js"></script><link href="/dist/css/arAHGThemeB5Plugin.bundle.7e0f865c257625fdb5ba.css" rel="stylesheet">
+    <?php if ($vendorJs): ?><script defer src="<?php echo $vendorJs; ?>"></script><?php endif; ?>
+    <?php if ($ahgJs): ?><script defer src="<?php echo $ahgJs; ?>"></script><?php endif; ?>
+    <?php if ($ahgCss): ?><link href="<?php echo $ahgCss; ?>" rel="stylesheet"><?php endif; ?>
     <?php echo get_component_slot('css'); ?>
   </head>
   <body class="d-flex flex-column min-vh-100 <?php echo $sf_context->getModuleName(); ?> <?php echo $sf_context->getActionName(); ?>">
