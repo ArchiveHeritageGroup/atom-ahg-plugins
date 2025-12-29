9d5d24afefe645a547a8c80eacd17aa81a37006c
Your Name <you@example.com>
2025-12-29 15:32:49 +0200

    Fix module names: ahgLibraryPlugin and ahgDAMPlugin

--- arAHGThemeB5Plugin/modules/menu/templates/_mainMenu.php
+++ arAHGThemeB5Plugin/modules/menu/templates/_mainMenu.php
@@ -81,10 +81,10 @@ $hasDam = checkPluginEnabled('arDAMPlugin') || checkPluginEnabled('ahgDAMPlugin'
             <li><a class="dropdown-item" href="<?php echo url_for(['module' => 'arGalleryPlugin', 'action' => 'add']); ?>"><i class="fas fa-images fa-fw me-2"></i><?php echo __('Gallery item'); ?></a></li>
           <?php endif; ?>
           <?php if ($hasLibrary): ?>
-            <li><a class="dropdown-item" href="<?php echo url_for(['module' => 'arLibraryPlugin', 'action' => 'add']); ?>"><i class="fas fa-book fa-fw me-2"></i><?php echo __('Library item'); ?></a></li>
+            <li><a class="dropdown-item" href="<?php echo url_for(['module' => 'ahgLibraryPlugin', 'action' => 'add']); ?>"><i class="fas fa-book fa-fw me-2"></i><?php echo __('Library item'); ?></a></li>
           <?php endif; ?>
           <?php if ($hasDam): ?>
-            <li><a class="dropdown-item" href="<?php echo url_for(['module' => 'arDAMPlugin', 'action' => 'add']); ?>"><i class="fas fa-photo-video fa-fw me-2"></i><?php echo __('Photo/DAM asset'); ?></a></li>
+            <li><a class="dropdown-item" href="<?php echo url_for(['module' => 'ahgDAMPlugin', 'action' => 'add']); ?>"><i class="fas fa-photo-video fa-fw me-2"></i><?php echo __('Photo/DAM asset'); ?></a></li>
           <?php endif; ?>
         <?php endif; ?>
 
