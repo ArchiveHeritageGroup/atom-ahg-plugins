75408c9ea9f202f69dce31415986eba4923ca4df
Your Name <you@example.com>
2026-01-07 14:35:08 +0200

    v1.6.73: Fix DAM install.sql - remove class_name from term inserts (column is on object table)

--- ahgDAMPlugin/data/install.sql
+++ ahgDAMPlugin/data/install.sql
@@ -350,8 +350,8 @@ SET @photo_exists = (SELECT t.id FROM term t JOIN term_i18n ti ON t.id=ti.id WHE
 INSERT INTO object (class_name, created_at, updated_at)
 SELECT 'QubitTerm', NOW(), NOW() FROM DUAL WHERE @photo_exists IS NULL;
 SET @photo_id = IF(@photo_exists IS NULL, LAST_INSERT_ID(), @photo_exists);
-INSERT INTO term (id, taxonomy_id, source_culture, class_name)
-SELECT @photo_id, 34, 'en', 'QubitTerm' FROM DUAL WHERE @photo_exists IS NULL;
+INSERT INTO term (id, taxonomy_id, code, source_culture)
+SELECT @photo_id, 34, '', 'en' FROM DUAL WHERE @photo_exists IS NULL;
 INSERT INTO term_i18n (id, culture, name)
 SELECT @photo_id, 'en', 'Photograph' FROM DUAL WHERE @photo_exists IS NULL;
 INSERT IGNORE INTO slug (object_id, slug) VALUES (@photo_id, 'photograph');
@@ -363,8 +363,8 @@ SET @audio_exists = (SELECT t.id FROM term t JOIN term_i18n ti ON t.id=ti.id WHE
 INSERT INTO object (class_name, created_at, updated_at)
 SELECT 'QubitTerm', NOW(), NOW() FROM DUAL WHERE @audio_exists IS NULL;
 SET @audio_id = IF(@audio_exists IS NULL, LAST_INSERT_ID(), @audio_exists);
-INSERT INTO term (id, taxonomy_id, source_culture, class_name)
-SELECT @audio_id, 34, 'en', 'QubitTerm' FROM DUAL WHERE @audio_exists IS NULL;
+INSERT INTO term (id, taxonomy_id, code, source_culture)
+SELECT @audio_id, 34, '', 'en' FROM DUAL WHERE @audio_exists IS NULL;
 INSERT INTO term_i18n (id, culture, name)
 SELECT @audio_id, 'en', 'Audio' FROM DUAL WHERE @audio_exists IS NULL;
 INSERT IGNORE INTO slug (object_id, slug) VALUES (@audio_id, 'audio');
@@ -375,8 +375,8 @@ SET @video_exists = (SELECT t.id FROM term t JOIN term_i18n ti ON t.id=ti.id WHE
 INSERT INTO object (class_name, created_at, updated_at)
 SELECT 'QubitTerm', NOW(), NOW() FROM DUAL WHERE @video_exists IS NULL;
 SET @video_id = IF(@video_exists IS NULL, LAST_INSERT_ID(), @video_exists);
-INSERT INTO term (id, taxonomy_id, source_culture, class_name)
-SELECT @video_id, 34, 'en', 'QubitTerm' FROM DUAL WHERE @video_exists IS NULL;
+INSERT INTO term (id, taxonomy_id, code, source_culture)
+SELECT @video_id, 34, '', 'en' FROM DUAL WHERE @video_exists IS NULL;
 INSERT INTO term_i18n (id, culture, name)
 SELECT @video_id, 'en', 'Video' FROM DUAL WHERE @video_exists IS NULL;
 INSERT IGNORE INTO slug (object_id, slug) VALUES (@video_id, 'video');
@@ -387,8 +387,8 @@ SET @image_exists = (SELECT t.id FROM term t JOIN term_i18n ti ON t.id=ti.id WHE
 INSERT INTO object (class_name, created_at, updated_at)
 SELECT 'QubitTerm', NOW(), NOW() FROM DUAL WHERE @image_exists IS NULL;
 SET @image_id = IF(@image_exists IS NULL, LAST_INSERT_ID(), @image_exists);
-INSERT INTO term (id, taxonomy_id, source_culture, class_name)
-SELECT @image_id, 34, 'en', 'QubitTerm' FROM DUAL WHERE @image_exists IS NULL;
+INSERT INTO term (id, taxonomy_id, code, source_culture)
+SELECT @image_id, 34, '', 'en' FROM DUAL WHERE @image_exists IS NULL;
 INSERT INTO term_i18n (id, culture, name)
 SELECT @image_id, 'en', 'Image' FROM DUAL WHERE @image_exists IS NULL;
 INSERT IGNORE INTO slug (object_id, slug) VALUES (@image_id, 'image');
@@ -399,8 +399,8 @@ SET @data_exists = (SELECT t.id FROM term t JOIN term_i18n ti ON t.id=ti.id WHER
 INSERT INTO object (class_name, created_at, updated_at)
 SELECT 'QubitTerm', NOW(), NOW() FROM DUAL WHERE @data_exists IS NULL;
 SET @data_id = IF(@data_exists IS NULL, LAST_INSERT_ID(), @data_exists);
-INSERT INTO term (id, taxonomy_id, source_culture, class_name)
-SELECT @data_id, 34, 'en', 'QubitTerm' FROM DUAL WHERE @data_exists IS NULL;
+INSERT INTO term (id, taxonomy_id, code, source_culture)
+SELECT @data_id, 34, '', 'en' FROM DUAL WHERE @data_exists IS NULL;
 INSERT INTO term_i18n (id, culture, name)
 SELECT @data_id, 'en', 'Dataset' FROM DUAL WHERE @data_exists IS NULL;
 INSERT IGNORE INTO slug (object_id, slug) VALUES (@data_id, 'dataset');
