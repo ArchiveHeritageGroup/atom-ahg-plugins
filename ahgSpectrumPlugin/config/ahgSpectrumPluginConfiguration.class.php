4ff976c7d44456a881e34960908923048ac5b278
Your Name <you@example.com>
2026-01-15 07:06:29 +0200

    Fix routing: Remove 4 duplicate routes from ahgSpectrumPlugin Configuration.php
    - Routes already defined in routing.yml
    - Removed addRoutes method entirely
    - Fixes part of issue #68

--- ahgSpectrumPlugin/config/ahgSpectrumPluginConfiguration.class.php
+++ ahgSpectrumPlugin/config/ahgSpectrumPluginConfiguration.class.php
@@ -1,5 +1,4 @@
 <?php
-
 class ahgSpectrumPluginConfiguration extends sfPluginConfiguration
 {
     public static $summary = 'Spectrum 5.0 Museum Procedures Plugin';
@@ -10,38 +9,15 @@ class ahgSpectrumPluginConfiguration extends sfPluginConfiguration
     
     // Plugins that depend on this one - will be disabled if this is disabled
     public static $dependents = ['ahgMuseumPlugin'];
-
+    
     public function initialize()
     {
-        $this->dispatcher->connect('routing.load_configuration', [$this, 'addRoutes']);
-    }
-
-    public function addRoutes(sfEvent $event)
-    {
-        $routing = $event->getSubject();
-
-        // Spectrum index route
-        $routing->prependRoute('spectrum_index', new sfRoute(
-            '/:slug/spectrum',
-            ['module' => 'spectrum', 'action' => 'index']
-        ));
-
-        // Spectrum workflow route
-        $routing->prependRoute('spectrum_workflow', new sfRoute(
-            '/spectrum/workflow',
-            ['module' => 'spectrum', 'action' => 'workflow']
-        ));
-
-        // Spectrum label route
-        $routing->prependRoute('spectrum_label', new sfRoute(
-            '/:slug/spectrum/label',
-            ['module' => 'spectrum', 'action' => 'label']
-        ));
-
-        // Spectrum dashboard route
-        $routing->prependRoute('spectrum_dashboard', new sfRoute(
-            '/spectrum/dashboard',
-            ['module' => 'spectrum', 'action' => 'dashboard']
-        ));
+        // Routes are defined in routing.yml - no duplicates needed here
+        
+        // Enable modules
+        $enabledModules = sfConfig::get('sf_enabled_modules', []);
+        $enabledModules[] = 'spectrum';
+        $enabledModules[] = 'api';
+        sfConfig::set('sf_enabled_modules', array_unique($enabledModules));
     }
 }
