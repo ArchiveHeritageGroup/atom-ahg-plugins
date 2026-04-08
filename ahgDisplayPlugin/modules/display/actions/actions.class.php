ae1d09c54dec81410f880f29f2c23b0e0f115e73
Johan Pieterse <johan@theahg.co.za>
2026-04-08 18:04:52 +0200

    Browse: use MPTT lft/rgt range to include record itself and all descendants

--- ahgDisplayPlugin/modules/display/actions/actions.class.php
+++ ahgDisplayPlugin/modules/display/actions/actions.class.php
@@ -501,7 +501,14 @@ class displayActions extends AhgController
         }
 
         if ($this->parentId) {
-            $query->where('io.parent_id', $this->parentId);
+            // Use MPTT lft/rgt range to include the record itself and all descendants
+            $ancestor = DB::table('information_object')->where('id', $this->parentId)->select('lft', 'rgt')->first();
+            if ($ancestor) {
+                $query->where('io.lft', '>=', $ancestor->lft)
+                      ->where('io.rgt', '<=', $ancestor->rgt);
+            } else {
+                $query->where('io.parent_id', $this->parentId);
+            }
         } elseif ($this->topLevelOnly === '1') {
             $query->where('io.parent_id', 1);
         }
