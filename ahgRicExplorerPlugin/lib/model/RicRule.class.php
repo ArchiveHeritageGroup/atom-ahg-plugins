8c123dee565306df824c03b9a20f92f483f463e8
Johan Pieterse <johan@theahg.co.za>
2026-04-09 08:20:26 +0200

    v3.21.94: Add RiC entity model stubs (RicActivity, RicInstantiation, RicPlace, RicRule)

---
+++ ahgRicExplorerPlugin/lib/model/RicRule.class.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * RiC Rule entity — stub model for Propel/AtoM object table compatibility.
+ * Created by the RiC sync process; must be instantiable by BaseObject::getFromRow().
+ */
+class RicRule extends QubitObject
+{
+}
